da8b0c08e3fba10bcad017f03fc29d7acd04dd78
derek-lws <13043998677@163.com>
2020-04-16 23:13:27 +0800

    day04 study python changed

--- src/day04.py
+++ src/day04.py
@@ -1,4 +1,7 @@
-import numpy as np
+import numpy as np#Numpy是Python的一种开源的数值计算扩展。
+#主要用于储存和处理大型矩阵
 import random
-a = [random.uniform(100.0,200.0) for i in range(50)]
-print(a)
+a = [random.uniform(100.0,200.0) for i in range(50)]#返回50个100.0-200.0之间的浮点数
+for x in a:
+	print(x)
+print(np.eye(2, dtype=int))#返回一个二维数组
